ceabde6a008063b28301f16fe9a307c9e11c65a3
Prateek Saxena <prtksxna@gmail.com>
2015-05-01 20:38:27 +0530

    Add the user model

---
+++ tests/auth.test.js
@@ -0,0 +1,7 @@
+var superagent = require('superagent');
+var expect = require('expect.js');
+
+describe( 'Authentication API', function () {
+	it( 'should let a user register with email and password' );
+	it( 'should let a user register with Google' );
+} );
