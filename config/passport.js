de3b8ffa74ad78fc438e0f83fd4ebbba96095705
Prateek Saxena <prtksxna@gmail.com>
2015-05-04 17:19:19 +0530

    Fix code style issues in passport config

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,10 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var mongoose = require('mongoose');
-var User = mongoose.model('User');
+var passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
+	mongoose = require('mongoose'),
+	User = mongoose.model('User');
 
 passport.use( new LocalStrategy(
-	function( email, password, done ) {
+	function ( email, password, done ) {
 		User.findOne( { email: email }, function ( err, user ) {
 			if ( err ) {
 				return done( err );
