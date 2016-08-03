f53d13c9ceaa7146398e0209afb60cb6b05729f2
Prateek Saxena <prtksxna@gmail.com>
2016-08-03 22:27:19 +0530

    docs: Add intro to config and db

--- config/config.js
+++ config/config.js
@@ -1,10 +1,17 @@
+// ## Configuration
+// > TODO: This isn't how the config should be, but it will do for
+// > now. We should probably do something like -
+// > http://www.chovy.com/node-js/managing-config-variables-inside-a-node-js-application/
 var config = {
 	dev: {
+		// > TODO: Rename this to hostname
 		api: 'http://localhost:3000/',
 		db: 'mongodb://localhost/graymatter',
-		secret: 'TODO: Secret'
+		// > TODO: In production this secret needs to be better and longer
+		secret: 'secret'
 	},
 	googleAuth: {
+		// > TOOD: These should be a secret and not checked into version control
 		clientID: '954841278741-tapbjff3qm8c67ubfhei1oal9jvsk0p8.apps.googleusercontent.com',
 		clientSecret: 'bD0kKRspQYs9vwdP7tKBnV98',
 		callbackURL: 'http://localhost:3000/sessions/google/callback'
--- models/db.js
+++ models/db.js
@@ -1,12 +1,18 @@
+// ## Database
+// We use mongoose, which is a driver for MongoDB.
+
 var
 	mongoose = require( 'mongoose' ),
 	config = require( '../config/config.js' );
 
-// TODO: How would we handle this in prod?
-// ANS: Environment variables
+// Mongoose's internal promise object is depracated.
 mongoose.Promise = global.Promise;
+// > TODO: This should change according to the environment
+// > in which we are running. The tests should also use a
+// > different database.
 mongoose.connect( config.dev.db );
 
+// Kill the database connection when the app is stopped.
 process.on( 'SIGINT', function () {
 	mongoose.connection.close( function () {
 		console.log( 'Mongoose connection terminated' );
