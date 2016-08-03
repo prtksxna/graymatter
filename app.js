fa725f0e273e2e2adaf6409113a74978a89a408e
Prateek Saxena <prtksxna@gmail.com>
2016-08-03 22:08:05 +0530

    docs: Add intro to app

--- app.js
+++ app.js
@@ -1,23 +1,34 @@
-require( './models/user' );
-require( './config/passport' );
+// # Graymatter
+// We generate a single page documentation for the server-side code
+// and this is the starting point. Refer to `Gruntfile.js` to find
+// out which files are included.
 
 var
+	// HTTP server and its modules
 	express = require( 'express' ),
 	logger = require( 'morgan' ),
 	bodyParser = require( 'body-parser' ),
+	// This file sets up a mongoose connection
 	mongoose = require( './models/db.js' ),
+	// Passport authenticaton and config
 	passport = require( 'passport' ),
+	user = require( './models/user' ),
+	passportConfig = require( './config/passport' ),
+	// Routes
 	index = require( './routes/index' ),
 	users = require( './routes/users' ),
 	sessions = require( './routes/sessions' ),
 	groups = require( './routes/groups' ),
+
 	app = express();
 
+// Setting up express middleware
 app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( passport.initialize() );
 
+// Setting up routes
 app.use( '/', index );
 app.use( '/users', users );
 app.use( '/sessions', sessions );
